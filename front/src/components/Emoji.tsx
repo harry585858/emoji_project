41144114c490c9ccd9a145ed07308c0ff4d98a37
Y <yangjaemin585858@gmail.com>
2025-05-02 11:45:37 +0900

    첫페이지완성

--- front/src/components/Emoji.tsx
+++ front/src/components/Emoji.tsx
@@ -2,20 +2,13 @@ import { useState } from 'react'
 import '../assets/Emoji.css'
 import myimg from '../assets/test.png'
 //이미지와 제목 제작자
-function Emoji() {
-  const [count, setCount] = useState(0)
+function Emoji({ src, alt }) {
   return (
-    <>
-    <li>
-      <div id='imgcontainer'>
-        <img src={myimg} alt='이미지 없음'>
-        </img>
-        <h3>제목</h3>
-        <p>제작자</p>
-      </div>
-      </li>
-    </>
-  )
+    <li className="emoji-item">
+      <img src={src} alt={alt} className="emoji-image" />
+    </li>
+  );
 }
 
-export default Emoji
+export default Emoji;
+
