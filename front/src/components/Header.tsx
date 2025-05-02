e3d5144d8c5e3013797958ac94421403fe0c0db1
Y <yangjaemin585858@gmail.com>
2025-05-02 12:27:32 +0900

    로그인회원가입완성

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -6,15 +6,15 @@ function Header() {
   const [login, setLogin] = useState(false)
   return (
     <>
-    <div id='header'>
-      <img alt='logo'></img>
+    <div id='header' >
+      <img alt='logo' onClick={() => { window.location.href = '/'; }}></img>
       <form action="/search">
       <input type="text" placeholder='검색어 입력'/>
       </form>
       {login ? (
   <button id='headerbtn' onClick={() => { window.location.href = '/mypage'; }}>마이페이지</button>
 ) : (
-  <button id='headerbtn' onClick={() => { window.location.href = '/login'; }}>로그인</button>
+  <button id='headerbtn' onClick={() => { window.location.href = '/account/login'; }}>로그인</button>
 )}
     </div>
     </>
