9da2296326efd468b06b2265b2b330e562857703
sooon3651 <lucia25834@gmail.com>
2025-05-12 05:41:12 +0900

    페이지 1차 추가제작
    마이페이지, 이미지상세, 이미지수정 페이지 구현
    이미지를 클릭하면 페이지 이동이 되지만 아직 마이페이지는 로그인 후 프로필 버튼이 없어서 연결해두지 않음

--- front/src/pages/router.tsx
+++ front/src/pages/router.tsx
@@ -2,12 +2,19 @@ import {Route, Routes} from "react-router-dom";
 import Login from "./Login";
 import Signin from "./Signin";
 import App from "../components/App";
+import Mypage from "./Mypage";
+import Detail from "./Detail";
+import Edit from "./Edit";
+
 function RouterConfig(){
     return(
         <Routes>
             <Route path="/account/login" element={<Login/>}/>
             <Route path="/account/signup" element={<Signin/>}/>
             <Route path="/" element={<App/>}/>
+            <Route path="/mypage" element={<Mypage />}/>
+            <Route path="/Detail" element={<Detail />}/>
+            <Route path="/Edit" element={<Edit />}/>
         </Routes>
     )
 }
