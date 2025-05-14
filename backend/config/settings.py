fcba58b891d8a00b5cc4e3c8ece77e3256fab548
DongHyeon Ko <hanser0204@naver.com>
2025-05-14 23:33:11 +0900

    DB path to env

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -94,14 +94,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ImageList',
-        'USER': 'admin',
-        'PASSWORD': '20020204',
-        'HOST': 'image-rds.c52o2goqgz9j.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '3306',
-    }
+    'default': env.db()
 }
 
 # Password validation
