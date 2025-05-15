e75b0050b1e6a0fdbd3e5c1383ba74cf23007b2f
kyh <project5886117@naver.com>
2025-05-15 12:16:17 +0900

    serializer name error

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,31 +11,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
-#environ part
-import environ
 import os
+import environ
+import pymysql
+pymysql.install_as_MySQLdb()
 
-from django.urls import reverse_lazy
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR,'.env')
-)
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
@@ -44,7 +31,6 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -58,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'accounts',  # 회원가입,로그인,로그아웃
+    'storages', #S3
     'image',
 ]
 
@@ -77,8 +64,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",   # 프론트 개발 주소필요
+    "http://localhost:8000",   # 프론트 개발 주소필요
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -100,20 +88,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': env.db(),
+    'default': env.db()
 }
 
 # Password validation
@@ -146,11 +124,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-#웹에서 접근할째 사용하는 경로
-MEDIA_URL = '/media/'
-#실제 서버에 저장되는 경로
-MEDIA_ROOT = BASE_DIR / 'media'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
@@ -160,3 +133,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#AWS S3 설정
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+#쟝고 최선 버전에선 아래와 같은 형식 사용
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
--- backend/image/serializers.py
+++ backend/image/serializers.py
@@ -2,17 +2,21 @@ from rest_framework import serializers
 from .models import Images
 
 class ImageSimpleSerializer(serializers.ModelSerializer):
+    imageURL = serializers.ImageField()
     class Meta:
         model = Images
         fields = ('imageID', 'title', 'imageURL')
 
 class ImageDetailSerializer(serializers.ModelSerializer):
+    imageURL = serializers.ImageField()
     class Meta:
         model = Images
         fields = ('imageID', 'userID', 'title', 'createDate', 'viewCount',
                   'imageURL')
 
 class ImageCreateSerializer(serializers.ModelSerializer):
+    imageURL = serializers.ImageField()
     class Meta:
         model = Images
         fields = ('userID', 'title','imageURL')
+        read_only_fields = ('userid',)  # userid body에서 입력 제외
