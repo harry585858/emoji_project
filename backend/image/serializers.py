87ff477ea76f78e91d5e371231f77a6f6c53e5b3
kyh <project5886117@naver.com>
2025-05-25 22:06:56 +0900

    add favorite flag

--- backend/image/serializers.py
+++ backend/image/serializers.py
@@ -1,25 +1,42 @@
 from rest_framework import serializers
-from .models import Images, Tags
+from .models import Images, Tags, Favoriteimages
 
 
 class ImageSimpleSerializer(serializers.ModelSerializer):
     imageURL = serializers.ImageField()
+    #즐겨찾기 여부
+    is_favorite = serializers.SerializerMethodField()
+
     class Meta:
         model = Images
-        fields = ('imageID', 'title', 'imageURL')
+        fields = ('imageID', 'title', 'imageURL','is_favorite')
+
+    def get_is_favorite(self, obj):
+        request = self.context.get('request', None)
+        if request and request.user.is_authenticated:
+            return Favoriteimages.objects.filter(userID=request.user, imageID=obj).exists()
+        return False
 
 class ImageDetailSerializer(serializers.ModelSerializer):
     imageURL = serializers.ImageField()
     #tags는 이미지DB에는 없기 때문에 tagDB에서 가져옴
     tags = serializers.SerializerMethodField()
+    # 즐겨찾기 여부
+    is_favorite = serializers.SerializerMethodField()
+
     class Meta:
         model = Images
         fields = ('imageID', 'userID', 'title', 'createDate', 'viewCount',
-                  'imageURL','tags')
+                  'imageURL','tags','is_favorite')
     # tags 역참조
     def get_tags(self, obj):
         return list(obj.tags_set.values_list('tag', flat=True))
 
+    def get_is_favorite(self, obj):
+        request = self.context.get('request', None)
+        if request and request.user.is_authenticated:
+            return Favoriteimages.objects.filter(userID=request.user, imageID=obj).exists()
+        return False
 
 class ImageCreateSerializer(serializers.ModelSerializer):
     imageURL = serializers.ImageField()
