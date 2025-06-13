c20339b56bdf58756591d41691bcb5e5fb07b25b
Y <87462160+harry585858@users.noreply.github.com>
2025-06-13 10:18:10 +0900

    Update Tagbox.tsx

--- front/src/components/Tagbox.tsx
+++ front/src/components/Tagbox.tsx
@@ -26,7 +26,7 @@ const TagBox = ({ imageId }: TagBoxProps) => {
 
         // 실제 API 호출
         const formdata = new FormData();
-        formdata.append('imageURL', 1);
+        formdata.append('imageURL', imageID.toString());
         const token = localStorage.getItem('access_token');
         const response = await axios.post(`${config.apiurl}image/tag/`,formdata, {
           headers: token ? { Authorization: `Bearer ${token}` } : {},
